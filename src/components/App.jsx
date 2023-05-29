ab58f929282ee3368ef4e507be80d12bb666f6b7
GIF1985 <125868361+GIF1985@users.noreply.github.com>
2023-05-29 23:48:40 +0300

    Поиск изображений

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,98 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import Searchbar from '../components/Searchbar/Searchbar';
+import ImageGallery from '../components/ImageGallery/ImageGallery';
+import Modal from '../components/Modal/Modal';
+import Button from '../components/Button/Button';
+import { BeatLoader } from 'react-spinners';
+import PropTypes from 'prop-types';
+
+const App = ({ API_KEY }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+
+  const handleSearch = query => {
+    setSearchQuery(query);
+    setPage(1);
+    setImages([]);
+    setError(null);
+  };
+
+  useEffect(() => {
+    if (!searchQuery) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    fetch(
+      `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    )
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setImages(prevImages => [...prevImages, ...data.hits]);
+      })
+      .catch(error => {
+        setError(error.message);
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 500);
+      });
+  }, [searchQuery, page, API_KEY]);
+
+  const handleImageClick = id => {
+    const selectedImage = images.find(image => image.id === id);
+    setSelectedImage(selectedImage);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
+
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Searchbar onSubmit={handleSearch} />
+      {isLoading && <BeatLoader color={'#123abc'} loading={true} />}
+      {error && <p>Error: {error}</p>}
+      {images.length > 0 && (
+        <ImageGallery
+          images={images}
+          onImageClick={handleImageClick}
+          isLoading={isLoading}
+        >
+          {selectedImage && (
+            <Modal
+              src={selectedImage.largeImageURL}
+              alt={selectedImage.tags}
+              onClose={handleCloseModal}
+            />
+          )}
+        </ImageGallery>
+      )}
+      {!isLoading && images.length === 0 && <p>Please enter a search query</p>}
+      {!isLoading && images.length > 0 && (
+        <Button label="Load more" onClick={handleLoadMore} />
+      )}
     </div>
   );
 };
+
+App.propTypes = {
+  API_KEY: PropTypes.string.isRequired,
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import App from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App API_KEY="36222156-a0a786a0445a35f0e3f8fc28d" />
   </React.StrictMode>
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App API_KEY="36222156-a0a786a0445a35f0e3f8fc28d" />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
